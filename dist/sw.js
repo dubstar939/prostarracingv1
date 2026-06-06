6a09edef8c94ac265f3948718f1396356f8c5820
dubstar939 <cwuthnow@gmail.com>
2026-06-06 14:45:40 -0500

    Added favicon.ico

---
+++ dist/sw.js
@@ -0,0 +1,32 @@
+// Service Worker for Pro Star-Racing - minimal implementation
+const CACHE_NAME = 'pro-star-racing-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  // Pass through all requests without caching for now
+  event.respondWith(fetch(event.request));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
